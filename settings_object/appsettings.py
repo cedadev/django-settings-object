0288a63cb77bb126fd530438871a0845a05789bc
Matt Pryor <matt.pryor@stfc.ac.uk>
2019-03-11 10:08:53 +0000

    Support for nested object factories

--- settings_object/appsettings.py
+++ settings_object/appsettings.py
@@ -4,10 +4,32 @@ Settings utilities for Django apps.
 """
 
 import re
+from functools import reduce
 
 from django.utils.module_loading import import_string
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.six import with_metaclass
+
+
+def import_callable(python_path):
+    """
+    Smarter version of Django's import_string that can deal with importing
+    nested classes and static method references.
+    """
+    module_parts = python_path.split('.')
+    attribute_parts = []
+    imported_mod = None
+    # Keep removing parts from the path until we find a module
+    while imported_mod is None and module_parts:
+        try:
+            imported_mod = import_string('.'.join(module_parts))
+        except ModuleNotFoundError:
+            attribute_parts.insert(0, module_parts.pop())
+    # If no module was found, raise a module not found error for the original path
+    if imported_mod is None:
+        _ = import_string(python_path)
+    # Otherwise, use getattr to traverse the attribute parts
+    return reduce(getattr, attribute_parts, imported_mod)
 
 
 class Setting(object):
@@ -15,9 +37,9 @@ class Setting(object):
     Property descriptor for a setting.
 
     Args:
-    default: Provides a default for the setting. If a callable is given, it
-             is called with the owning py:class:`SettingsObject` as it's only
-             argument. Defaults to ``NO_DEFAULT``.
+        default: Provides a default for the setting. If a callable is given, it
+                 is called with the owning py:class:`SettingsObject` as it's only
+                 argument. Defaults to ``NO_DEFAULT``.
     """
     #: Sentinel object representing no default. A sentinel is required because
     #: ``None`` is a valid default value.
@@ -30,7 +52,7 @@ class Setting(object):
         self.name = name
 
     def __get__(self, instance, owner):
-        # Settings should be accessed as instance attributes
+        # Settings should be accessed as instance attributes
         if not instance:
             raise TypeError('Settings cannot be accessed as class attributes')
         try:
@@ -39,13 +61,13 @@ class Setting(object):
             return self._get_default(instance)
 
     def _get_default(self, instance):
-        # This is provided as a separate method for easier overriding
+        # This is provided as a separate method for easier overriding
         if self.default is self.NO_DEFAULT:
             raise ImproperlyConfigured('Required setting: {}.{}'.format(instance.name, self.name))
         return self.default(instance) if callable(self.default) else self.default
 
     def __set__(self, instance, value):
-        # This method exists so that the descriptor is considered a data-descriptor
+        # This method exists so that the descriptor is considered a data-descriptor
         raise AttributeError('Settings are read-only')
 
 
@@ -55,7 +77,7 @@ class SettingsMeta(type):
     """
     def __new__(self, name, bases, namespace):
         klass = super(SettingsMeta, self).__new__(self, name, bases, namespace)
-        # For every attribute that is a Setting, set it's name
+        # For every attribute that is a Setting, set it's name
         for prop_name, prop in namespace.items():
             if isinstance(prop, Setting):
                 prop.__set_name__(klass, prop_name)
@@ -85,7 +107,7 @@ class ImportStringSetting(Setting):
     imported.
     """
     def __get__(self, instance, owner):
-        return import_string(
+        return import_callable(
             super(ImportStringSetting, self).__get__(instance, owner)
         )
 
@@ -104,36 +126,64 @@ class ObjectFactorySetting(Setting):
     The ``FACTORY`` can either be a constructor or a dedicated factory function.
 
     Keys in ``PARAMS`` are lower-cased and used as ``kwargs`` for the factory.
+
+    Object factory settings can be nested, so that a parameter of an object factory
+    can be another object factory.
     """
     MISSING_ARG_REGEX = r"missing \d+ required positional arguments?: "
     INVALID_ARG_MATCH = "got an unexpected keyword argument"
     ARG_NAME_REGEX = r"'(\w+)'"
 
-    def __get__(self, instance, owner):
-        factory_definition = super(ObjectFactorySetting, self).__get__(instance, owner)
-        factory = import_string(factory_definition['FACTORY'])
-        kwargs = { k.lower(): v for k, v in factory_definition['PARAMS'].items() }
-        # We want to convert type errors for missing or invalid arguments into
-        # errors about missing or invalid settings
-        try:
-            return factory(**kwargs)
-        except TypeError as exc:
-            message = str(exc)
-            if re.search(self.MISSING_ARG_REGEX, message):
-                required = [
-                    '{}.{}.PARAMS.{}'.format(instance.name, self.name, name.upper())
-                    for name in re.findall(self.ARG_NAME_REGEX, message)
-                ]
-                raise ImproperlyConfigured(
-                    'Required setting(s): {}'.format(', '.join(required))
-                )
-            elif self.INVALID_ARG_MATCH in message:
-                match = re.search(self.ARG_NAME_REGEX, message)
-                raise ImproperlyConfigured(
-                    'Invalid setting: {}.{}.PARAMS.{}'.format(
-                        instance.name, self.name, match.group(1).upper()
+    def _process_item(self, item, prefix):
+        # If the item is a factory dict, do some processing
+        if isinstance(item, dict) and 'FACTORY' in item:
+            factory = import_callable(item['FACTORY'])
+            # Process the params for nested factory definitions
+            kwargs = {
+                k.lower(): self._process_item(v, '{}.PARAMS.{}'.format(prefix, k))
+                for k, v in item['PARAMS'].items()
+            }
+            # We want to convert type errors for missing or invalid arguments into
+            # errors about missing or invalid settings
+            try:
+                return factory(**kwargs)
+            except TypeError as exc:
+                message = str(exc)
+                if re.search(self.MISSING_ARG_REGEX, message):
+                    required = [
+                        '{}.PARAMS.{}'.format(prefix, name.upper())
+                        for name in re.findall(self.ARG_NAME_REGEX, message)
+                    ]
+                    raise ImproperlyConfigured(
+                        'Required setting(s): {}'.format(', '.join(required))
                     )
-                )
-            else:
-                # Re-raise any other type error
-                raise
+                elif self.INVALID_ARG_MATCH in message:
+                    match = re.search(self.ARG_NAME_REGEX, message)
+                    raise ImproperlyConfigured(
+                        'Invalid setting: {}.PARAMS.{}'.format(
+                            prefix, match.group(1).upper()
+                        )
+                    )
+                else:
+                    # Re-raise any other type error
+                    raise
+        # For any other dict, convert the values if required
+        if isinstance(item, dict):
+            return {
+                k: self._process_item(v, '{}.{}'.format(prefix, k))
+                for k, v in item.items()
+            }
+        # For a list or tuple, convert the elements if required
+        if isinstance(item, (list, tuple)):
+            return [
+                self._process_item(v, '{}[{}]'.format(prefix, i))
+                for i, v in enumerate(item)
+            ]
+        # For anything else, just return the item
+        return item
+
+    def __get__(self, instance, owner):
+        return self._process_item(
+            super(ObjectFactorySetting, self).__get__(instance, owner),
+            '{}.{}'.format(instance.name, self.name)
+        )
