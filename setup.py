2b66c0fc5eae92972df5210b4bc43f7d95ad9ceb
Matt Pryor <matt.pryor@stfc.ac.uk>
2021-02-16 18:32:30 +0000

    Use pyproject.toml for setuptool_scm

--- setup.py
+++ setup.py
@@ -1,32 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import os
-from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
+import setuptools
 
 if __name__ == "__main__":
-    setup(
-        name = 'django-settings-object',
-        setup_requires = ['setuptools_scm'],
-        use_scm_version = True,
-        description = 'Django utilities for building settings objects.',
-        long_description = README,
-        classifiers = [
-            "Programming Language :: Python",
-            "Framework :: Django",
-        ],
-        author = 'Matt Pryor',
-        author_email = 'matt.pryor@stfc.ac.uk',
-        url = 'https://github.com/cedadev/django-settings-object',
-        keywords = 'web django settings',
-        packages = find_packages(),
-        include_package_data = True,
-        zip_safe = False,
-        extras_require = {
-            'django': ['django'],
-        }
-    )
+    setuptools.setup()
